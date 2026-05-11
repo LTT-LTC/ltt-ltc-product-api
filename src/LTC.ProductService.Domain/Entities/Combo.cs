64edaee2b0088ec614920232ff3d8482e7a0e5cf
Dranov220805 <duongthanhlong220805@gmail.com>
2026-05-11 14:24:41 +0700

    fix: fix multiple bug in all domain

--- src/LTC.ProductService.Domain/Entities/Combo.cs
+++ src/LTC.ProductService.Domain/Entities/Combo.cs
@@ -10,8 +10,8 @@ namespace LTC.ProductService.Entities
         public Guid? TenantId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public decimal TotalPrice { get; set; }
-        public decimal BasePrice { get; set; }
+        public decimal? TotalPrice { get; set; }
+        public decimal? BasePrice { get; set; }
         public bool IsActive { get; set; }
         public string ImageUrl { get; set; }
         /// <summary>
